9f2f195fe1080e8476819c548c6c282e42a71dad
janearisah <janet@chaka.ng>
2019-11-19 17:24:48 +0100

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,13 +1,30 @@
-import Vue from 'vue';
-import App from './App.vue';
-import './registerServiceWorker';
-import router from './router';
-import store from './store';
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+import "lazysizes";
+import "lazysizes/plugins/parent-fit/ls.parent-fit";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App),
-}).$mount('#app');
+  created() {
+    AOS.init({
+      duration: 500,
+      delay: 100,
+      easing: "ease-in-out",
+      mirror: false,
+      startEvent: "DOMContentLoaded",
+      anchorPlacement: "top-center",
+      // once: true,
+      offset: 50
+    });
+  },
+  render: h => h(App)
+}).$mount("#app");
