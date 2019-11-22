64fc0d8ffa2302e6e8881a85aa7be374ec8ac2dc
janearisah <janet@chaka.ng>
2019-11-22 14:25:20 +0100

    implemented statements/history pages

--- src/views/dashboard/accounts/Overview.vue
+++ src/views/dashboard/accounts/Overview.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <section class="accounts__title">
-      <h2>Overview</h2>
+      <h3>Overview</h3>
     </section>
     <section class="accounts-overview">
       <div class="accounts-overview__text">
@@ -26,7 +26,38 @@
     </section>
 
     <section class="accounts-overview__cards">
-      <Card title="Trading Status" class="accounts-overview__card"> Content</Card>
+      <Card title="Trading Status" class="accounts-overview__card">
+        <div class="accounts-overview__flex">
+          <img
+            class="accounts-overview__symbol"
+            src="../../../assets/img/accounts-naira.svg"
+            alt="Naira"
+            width="16px"
+          />
+          <span class="accounts-overview__currency">Local</span>
+          <span class="accounts-overview__approved">Approved</span>
+          <img
+            class="accounts-overview__approved--img"
+            src="../../../assets/img/checked.svg"
+            alt="Checked"
+          />
+        </div>
+        <div class="accounts-overview__flex">
+          <img
+            class="accounts-overview__symbol"
+            src="../../../assets/img/accounts-dollar.svg"
+            alt="Dollar"
+            width="16px"
+          />
+          <span class="accounts-overview__currency">Global</span>
+          <span class="accounts-overview__approved">Approved</span>
+          <img
+            class="accounts-overview__approved--img"
+            src="../../../assets/img/checked.svg"
+            alt="Checked"
+          />
+        </div>
+      </Card>
       <Card
         title="Stamps Earned"
         :action="{ text: 'See All', link: 'accounts-stamps' }"
@@ -38,6 +69,14 @@
         :action="{ text: 'Edit Profile', link: 'accounts-settings' }"
         class="accounts-overview__card"
       >
+        <div class="accounts-overview__flex">
+          <span class="accounts-overview__currency">Email</span>
+          <span class="accounts-overview__approved">jane@doe.com</span>
+        </div>
+        <div class="accounts-overview__flex">
+          <span class="accounts-overview__currency">Phone No</span>
+          <span class="accounts-overview__approved">0901284928</span>
+        </div>
       </Card>
     </section>
   </div>
--- src/views/dashboard/accounts/Statements.vue
+++ src/views/dashboard/accounts/Statements.vue
@@ -1,5 +1,97 @@
 <template>
-  <section>
-    Statements
-  </section>
+  <div>
+    <section class="accounts__title">
+      <h3>History</h3>
+      <select class="form__input" name="" id="">
+        <option value="1">All</option>
+        <option value="2">Local</option>
+        <option value="3">Global</option>
+      </select>
+    </section>
+    <section class="accounts-statements__all">
+      <button
+        class="btn btn-block"
+        v-for="(button, index) in statementButtons"
+        :key="index"
+        @click="activeButton = button.value"
+        :class="{ active: activeButton === button.value }"
+      >
+        {{ button.name }}
+      </button>
+    </section>
+    <section class="accounts-statements__date">
+      <input class="form__input" type="date" name="start" />to
+      <input class="form__input" type="date" name="start" />
+    </section>
+
+    <section class="accounts-statements__downloads">
+      <StatementsCard v-for="(statement, i) in statements" :key="i" :statement="statement" />
+    </section>
+  </div>
 </template>
+
+<script>
+import StatementsCard from "../../../components/accounts/StatementsCard";
+export default {
+  name: "accounts-statements",
+  components: {
+    StatementsCard
+  },
+  data() {
+    return {
+      activeButton: 1,
+      statementButtons: [
+        {
+          name: "Statement",
+          value: 1
+        },
+        {
+          name: "Confirmations",
+          value: 2
+        },
+        {
+          name: "Tax",
+          value: 3
+        }
+      ],
+      statements: [
+        {
+          description: "Description",
+          detail: "2018 Tax Year 1099-B",
+          link: ""
+        },
+        {
+          description: "Description",
+          detail: "2018 Tax Year 1099-B",
+          link: ""
+        },
+        {
+          description: "Description",
+          detail: "2018 Tax Year 1099-B",
+          link: ""
+        },
+        {
+          description: "Description",
+          detail: "2018 Tax Year 1099-B",
+          link: ""
+        },
+        {
+          description: "Description",
+          detail: "2018 Tax Year 1099-B",
+          link: ""
+        },
+        {
+          description: "Description",
+          detail: "2018 Tax Year 1099-B",
+          link: ""
+        },
+        {
+          description: "Description",
+          detail: "2018 Tax Year 1099-B",
+          link: ""
+        }
+      ]
+    };
+  }
+};
+</script>
