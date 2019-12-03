4884c0da7542e478633f1794184c4870d56cc99c
janearisah <janet@chaka.ng>
2019-12-03 18:16:21 +0100

    implemented check for dollar funding

--- src/views/dashboard/Categories.vue
+++ src/views/dashboard/Categories.vue
@@ -1,53 +1,74 @@
 <template>
     <Fragment>
-        <Tags 
-        :taglists="gettagslistsArray"/>
-        <hr class="division-logger"/>
-        <Instrumentbase
-         :loadercardState="cardState"
-         :instrumentsList="getInstrumentsListArray"/>
+        <Tags :taglists="gettagslistsArray" />
+        <hr class="division-logger" />
+        <template v-if="getWindowWidth === 'desktop'">
+            <section v-if="getInstrumentsListArray.length > 0" class="instrument-base">
+                <InstrumentCard
+                    v-for="(instrument, index) in getInstrumentsListArray"
+                    :key="index"
+                    :instrument="instrument"
+                />
+            </section>
+        </template>
+        <section v-else>
+            <template v-if="getInstrumentsListArray.length > 0">
+                <InstrumentMobile
+                    v-for="(instrument, index) in getInstrumentsListArray"
+                    :key="index"
+                    :instrument="instrument"
+            /></template>
+        </section>
+        <!-- <Instrumentbase :loadercardState="cardState" :instrumentsList="getInstrumentsListArray" /> -->
     </Fragment>
 </template>
 <script>
-import { Fragment } from 'vue-fragment';
-import Instrumentbase from '../../components/Instrument/instrumentbase';
-import Tags from '../../components/Tags';
-import { mapGetters, mapActions, mapMutations } from 'vuex';
+import { Fragment } from "vue-fragment";
+import InstrumentCard from "../../components/Instrument/InstrumentCard";
+import InstrumentMobile from "../../components/watchlist/MobileWatchlist";
+import Instrumentbase from "../../components/Instrument/instrumentbase";
+import Tags from "../../components/Tags";
+import { mapGetters, mapActions, mapMutations } from "vuex";
 
 export default {
-    name: 'Categories',
-    data(){
-        return{
-            cardState:true
-        }
+    name: "Categories",
+    data() {
+        return {
+            cardState: true
+        };
     },
     components: {
         Fragment,
         Instrumentbase,
-        Tags
+        Tags,
+        InstrumentCard,
+        InstrumentMobile
     },
-     computed:{
-        ...mapGetters(['gettagslistsArray','getInstrumentsListArray']),
+    computed: {
+        ...mapGetters(["gettagslistsArray", "getInstrumentsListArray", "getWindowWidth"])
     },
-    methods:{
-        ...mapMutations(['SET_TAGS_PAYLOAD__INSTRUMENT_BY_TAGS']),
-         ...mapActions(['GET_TAGS_CATEGORIES','GET_INSTRUMENT_BY_TAGS']),
-            mountingActions(){
-                
-                this.$store.dispatch('GET_TAGS_CATEGORIES').then(()=>{
-                    const payloadGetInstrument = {symbols:this.gettagslistsArray[0].Instruments}
-                     this.cardState = true;
-                    this.SET_TAGS_PAYLOAD__INSTRUMENT_BY_TAGS(this.gettagslistsArray[0].Instruments ? this.gettagslistsArray[0].Instruments : null );
-                    this.cardState = true;
-                    this.GET_INSTRUMENT_BY_TAGS(payloadGetInstrument).then((response)=>{
-                        this.cardState = response;
-                        console.log('here is the gertter???????',this.getInstrumentsListArray)
-                    });
+    methods: {
+        ...mapMutations(["SET_TAGS_PAYLOAD__INSTRUMENT_BY_TAGS"]),
+        ...mapActions(["GET_TAGS_CATEGORIES", "GET_INSTRUMENT_BY_TAGS"]),
+        mountingActions() {
+            this.$store.dispatch("GET_TAGS_CATEGORIES").then(() => {
+                const payloadGetInstrument = { symbols: this.gettagslistsArray[0].Instruments };
+                this.cardState = true;
+                this.SET_TAGS_PAYLOAD__INSTRUMENT_BY_TAGS(
+                    this.gettagslistsArray[0].Instruments
+                        ? this.gettagslistsArray[0].Instruments
+                        : null
+                );
+                this.cardState = true;
+                this.GET_INSTRUMENT_BY_TAGS(payloadGetInstrument).then(response => {
+                    this.cardState = response;
+                    console.log("here is the gertter???????", this.getInstrumentsListArray);
                 });
-            }
+            });
+        }
     },
-    mounted(){
-        this.mountingActions()
+    mounted() {
+        this.mountingActions();
     }
 };
 </script>
