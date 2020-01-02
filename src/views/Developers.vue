cbc973349d920fb1391d71054e2cbf95ab2fd74a
Olaolu Ajose <olaolumusic@yahoo.com>
2020-01-02 13:15:50 +0100

    developer page with added assets.

--- src/views/Developers.vue
+++ src/views/Developers.vue
@@ -1,41 +1,94 @@
 <template>
-    <main class="main" role="main">
-        <div class="auth-box">
-            <div class="text-center">
-                <div class="form__header">
-                    <div class="form__image">
-                        <svg
-                            width="83"
-                            class="stock-vdr__svg"
-                            height="83"
-                            viewBox="0 0 83 83"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                d="M41.029 0C18.3668 0 0.00195312 18.3697 0.00195312 41.027C0.00195312 63.6868 18.3691 82.054 41.029 82.054C63.6862 82.054 82.0534 63.6868 82.0534 41.027C82.0534 18.3723 63.6862 0 41.029 0ZM41.029 74.3613C22.6182 74.3613 7.69464 59.4378 7.69464 41.027C7.69464 22.6162 22.6208 7.69269 41.029 7.69269C59.4371 7.69269 74.3607 22.6162 74.3607 41.027C74.3607 59.4378 59.4374 74.3613 41.029 74.3613ZM57.1527 23.5752C55.4089 22.5342 53.1807 23.1521 52.1782 24.9598L38.4212 49.6709L30.7595 40.3449C29.3723 38.7474 27.5287 37.9193 25.7876 38.9603C24.0438 40.0038 23.3233 42.5143 24.4517 44.1247L35.1136 57.1019C35.911 58.1378 36.8212 58.7585 37.7727 58.9355L37.7804 58.951L37.8779 58.9561C38.0934 58.9894 41.3141 59.5843 42.7013 57.1022L58.4917 28.7399C59.4936 26.929 58.8964 24.6187 57.1527 23.5752Z"
-                                fill="#3DB39E"
-                            />
-                        </svg>
-                    </div>
-                    <h4 class="form__title--green">Verification Email Sent</h4>
-                    <p class="form__caption mt-2">
-                        We have sent you an email with a link to verify your registered email
-                        address.
-                    </p>
-                </div>
-                <div class="mt-3">
-                    <router-link class="btn btn__primary" :to="{ name: 'login' }"
-                        >Continue to site</router-link
-                    >
-                </div>
-            </div>
-        </div>
-    </main>
+	<main class="main" role="main">
+		<section class="section hero">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-5 hero__body">
+						<div>
+							<h2 class="hero__title">Integrate Chaka into your application, service or workflow</h2>
+							<p
+								class="hero__text"
+							>To use Chaka’s API and integrate with a wide selection of capital market tools, reach out to us</p>
+						 
+						</div>
+					</div>
+					<div class="col-md-7">
+						<div class="hero__img">
+							<picture>
+								<source media="(max-width: 640px)" srcset="../assets/img/code-mobile.png" />
+								<img src="../assets/img/code.png" alt />
+							</picture>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section> 
+
+		<section class="section section--pad">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+						<aside class="section__side">
+							<div class="section__media">
+								<img src="../assets/img/ad.png" alt />
+							</div>
+							<div class="section__logo-banner">
+								<img src="../assets/img/logo-large.svg" alt />
+								<a href="mailto:support@chaka.ng">support@chaka.ng</a>
+							</div>
+						</aside>
+					</div>
+					<div class="col-md-8">
+						<form class action>
+							<div class="form__header form__header--left">
+								<h3 class="form__title">Send us a message</h3>
+							</div>
+							<div class="form-row">
+								<div class="form-group col-md-6">
+									<label for="name">Full Name</label>
+									<input type="text" class="form-control" id="name" placeholder />
+								</div>
+								<div class="form-group col-md-6">
+									<label for="email">Your email</label>
+									<input type="email" class="form-control" id="email" placeholder />
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="subject">Subject</label>
+								<input type="text" class="form-control" id="subject" placeholder />
+							</div>
+							<div class="form-group">
+								<label for="message"></label>
+								<textarea
+									class="form-control"
+									name="message"
+									id="message"
+									cols="30"
+									rows="10"
+									placeholder="Message"
+								></textarea>
+							</div>
+							<button class="btn btn__grey btn--wide">SEND A MESSAGE</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</section>
+	</main>
 </template>
 
 <script>
 export default {
-    name: 'VerificationSent'
+	name: 'Developers',
+	mounted() {
+		document.title = 'Chaka - Developers';
+		document.getElementsByTagName('meta')['keywords'].content =
+			'nigerian stock exchange, US stock market, nigeria stock market, online investment, investing, capital market, stock trading, stockbroker, stocks, shares, investment passport, chaka, nse, nyse';
+		document.getElementsByTagName('meta')['description'].content =
+			'Integrate Chaka into your application, service or workflow';
+		// document.getElementsByTagName('meta')['og:title'].content = 'Chaka - Developers';
+		// document.getElementsByTagName('meta')['og:description'].content =
+		// 	'Integrate Chaka into your application, service or workflow';
+	}
 };
 </script>
