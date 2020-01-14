3ed6bae9069bf7dbbc976922e9ee2cd77c1719a4
janearisah <janet@chaka.ng>
2020-01-14 17:41:03 +0100

    image compressions on home page images

--- src/router/index.js
+++ src/router/index.js
@@ -18,18 +18,18 @@ const router = new VueRouter({
 router.beforeEach((to, from, next) => {
     store.commit("RESET_REQ");
     store.commit("RESET_MODALS");
-    store.commit("RESET_COMPONENT_LOADER",true);
+    store.commit("RESET_COMPONENT_LOADER", true);
     store.commit("SEARCH_OPENED", false);
     next();
 });
 
 router.afterEach((to, from) => {
-    store.commit("RESET_COMPONENT_LOADER",false);
+    store.commit("RESET_COMPONENT_LOADER", false);
     store.commit("SET_SHOW_NAVBAR_KYC", true);
 });
 router.onError(error => {
     console.log("Router Error:::", error);
-    if (/loading chunk \d* failed./i.test(error.message)) {
+    if (error.startsWith("ChunkLoadError:")) {
         window.location.reload();
     }
 });
