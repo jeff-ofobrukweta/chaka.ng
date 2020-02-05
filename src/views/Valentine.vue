1b237c8c6117d15baad282828a9d03187869bd73
janearisah <janet@chaka.ng>
2020-02-05 14:12:35 +0100

    Test for encrypt function on love calculator

--- src/views/Valentine.vue
+++ src/views/Valentine.vue
@@ -282,7 +282,10 @@ export default {
             }
         },
         cipher(salt) {
-            const textToChars = text => text.split("").map(c => c.charCodeAt(0));
+            const textToChars = text => {
+                const temp = text.split("");
+                return temp.map(c => c.charCodeAt(0));
+            };
             const byteHex = n => ("0" + Number(n).toString(16)).substr(-2);
             const applySaltToChar = code => textToChars(salt).reduce((a, b) => a ^ b, code);
 
