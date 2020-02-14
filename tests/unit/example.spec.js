ab9f376f1fb13595e4558fd7bc9f24be19c758ed
jeff-ofobrukweta <oghenerukevwejeff@gmail.com>
2020-02-14 06:03:26 -0800

    added the unit test snapshot for news

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,13 +1,33 @@
 import { shallowMount,mount } from '@vue/test-utils';
 import NewsCard from '@/components/cards/NewsCard.vue';
 
+
+
+/*
+Inputs
+
+Data: the component’s data
+Props: the component’s props
+User Interaction: user clicks a button
+Lifecycle Methods: mounted(), created(), etc.
+Store: state management data
+Route Params: data in the route
+*/
+
+
+
+// test the news card and check for images link is valid
 describe('NewsCard-vue', () => {
     it('renders props.news when passed', () => {
-        const news = { message: 'foo' }
-        const wrapper = shallowMount(NewsCard,{
-            propsData: { news }
+        const news = {
+            imageUrl:"https://chaka.ng/dashboard/singlestock/BABA",
+            name:"news",
+            title:"title"
+        }
+        const wrapper = mount(NewsCard,{
+            propsData: { news}
           })
-        expect(wrapper.find('h5'),text())
+          expect(wrapper).toMatchSnapshot()
     });
 });
 
