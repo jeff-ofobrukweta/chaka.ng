b6b32dc0351adb30bf2fea7b21895f0f2283ff3a
jeff-ofobrukweta <oghenerukevwejeff@gmail.com>
2020-02-25 07:59:19 -0800

    added google and facebook social oauth

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -145,10 +145,11 @@ const actions = {
             api.post("/auth/social-auth", payload).then(
                 resp => {
                     if (resp.status >= 200 && resp.status < 400) {
-                        // localStorage.setItem("AUTH_TOKEN", resp.data.data.token);
-                        // localStorage.setItem("REFRESH_TOKEN", resp.data.data.refreshToken);
-                        // commit("SET_LOGGED_IN", true);
-                        // commit("REQ_SUCCESS", null, { root: true });
+                        // console.log('email meant here new 2kkkkkkk>>>>>>>>>>>>>',resp)
+                        localStorage.setItem("AUTH_TOKEN", resp.data.data.token);
+                        localStorage.setItem("REFRESH_TOKEN", resp.data.data.refreshToken);
+                        commit("SET_LOGGED_IN", true);
+                        commit("REQ_SUCCESS", null, { root: true });
                         // console.log('email meant here new 2>>>>>>>>>>>>>',resp)
                         fbq('track', 'login');
                         resolve(true);
